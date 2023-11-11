22b9fd54afadf32a7f4828a28680d8b40d2a7f9e
devetechnologies <devetechnologies@gmail.com>
2023-11-11 10:40:28 -0400

    add CacheNetworkImage and velocity param

--- lib/cookbooks/views/animations/physics_similation/physics_card_drag.dart
+++ lib/cookbooks/views/animations/physics_similation/physics_card_drag.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:devetechnologies_cookbook/cookbooks/views/animations/physics_similation/widgets/draggable_card.dart';
 import 'package:flutter/material.dart';
 
@@ -9,10 +10,36 @@ class PhysicsCardDrag extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const DraggAbleCard(
-        child: FlutterLogo(
-          size: 150,
-        ),
+      body: DraggAbleCard(
+        child: CachedNetworkImage(
+          key: UniqueKey(),
+          imageUrl:
+              'https://carwow-uk-wp-3.imgix.net/18015-MC20BluInfinito-scaled-e1666008987698.jpg',
+          imageBuilder: (context, imageProvider) => Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  image:
+                      DecorationImage(image: imageProvider, fit: BoxFit.fill))),
+          placeholder: (context, url) => Container(
+            alignment: Alignment.center,
+            child: const FadeInImage(
+              image: NetworkImage(
+                  'https://carwow-uk-wp-3.imgix.net/18015-MC20BluInfinito-scaled-e1666008987698.jpg'),
+              placeholder: AssetImage('assets/images/jar-loading.gif'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ), /*FadeInImage(
+        //width: 200,
+        //height: 200,
+        placeholder: AssetImage('assets/images/no-image.png'),
+        image: NetworkImage(
+            'https://carwow-uk-wp-3.imgix.net/18015-MC20BluInfinito-scaled-e1666008987698.jpg'),
+        fit: BoxFit.cover,
+      )*/
       ),
     );
   }
--- lib/cookbooks/views/animations/physics_similation/widgets/draggable_card.dart
+++ lib/cookbooks/views/animations/physics_similation/widgets/draggable_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 
 class DraggAbleCard extends StatefulWidget {
   final Widget child;
@@ -8,12 +9,73 @@ class DraggAbleCard extends StatefulWidget {
   State<DraggAbleCard> createState() => _DraggAbleCardState();
 }
 
-class _DraggAbleCardState extends State<DraggAbleCard> {
+class _DraggAbleCardState extends State<DraggAbleCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  Alignment _aliment = Alignment.center;
+  late Animation<Alignment> _animation;
+
+  void _runAnimation(Offset pixelsPerSecond, Size size) {
+    _animation = _controller
+        .drive(AlignmentTween(begin: _aliment, end: Alignment.center));
+    // Calculate the velocity relative to the unit interval, [0,1],
+    const spring = SpringDescription(
+      mass: 25,
+      stiffness: 1,
+      damping: 1,
+    );
+    // used by the animation controller.
+    final unitsPerSecondX = pixelsPerSecond.dx / size.width;
+    final unitsPerSecondY = pixelsPerSecond.dy / size.height;
+    final unitsPerSecond = Offset(unitsPerSecondX, unitsPerSecondY);
+    final unitVelocity = unitsPerSecond.distance;
+
+    final simulation = SpringSimulation(spring, 0, 1, -unitVelocity);
+
+    _controller.animateWith(simulation);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    _controller.addListener(() {
+      setState(() {
+        _aliment = _animation.value;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Align(
-      child: Card(
-        child: widget.child,
+    var size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onPanDown: (details) {
+        _controller.stop();
+      },
+      onPanUpdate: (details) {
+        setState(() {
+          _aliment += Alignment(details.delta.dx / size.width / 2,
+              details.delta.dy / size.width / 2);
+          print(_aliment);
+        });
+      },
+      onPanEnd: (details) {
+        _runAnimation(details.velocity.pixelsPerSecond, size);
+      },
+      child: Align(
+        alignment: _aliment,
+        child: Card(
+          child: widget.child,
+        ),
       ),
     );
   }
